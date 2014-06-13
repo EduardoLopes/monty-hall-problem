0e108145c1f9311a7f97c234429cc96cc37ae7db
Eduardo Lopes <EduardoLopesDS@gmail.com>
2014-06-13 19:25:42 -0300

    Add the logic for the game

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,105 @@
+(function() {
+  'use strict';
 
+  var Monty = (function() {
+
+    var firstChosenDoor,
+        secondChosenDoor,
+        i,
+        doors,
+        choice;
+
+    function random ( min, max ) {
+      return ( min + ( Math.random() * ( max - min ) ) );
+    };
+
+    function generateDoors () {
+      return [ 'car', 'zonk', 'zonk' ].sort(function() {
+        return Math.round( random(-1, 2) );
+      });
+    }
+
+    function chooseDoor( id ) {
+      document.getElementById( id ).style.background = '#00A9C5';
+      if( secondChosenDoor ){
+        document.getElementById( firstChosenDoor ).style.background = '#70401C';
+      }
+    }
+
+    function openFirstDoor(){
+
+    }
+
+    function openDoors(){
+      for ( i = 0; i < doors.length; i++ ) {
+
+        if( doors[ i ] === 'car' && choice === 'door-' + i ){
+          document.getElementById( 'door-' + i ).style.background = '#08B400';
+        } else if( doors[ i ] === 'car' ) {
+          document.getElementById( 'door-' + i ).style.background = '#00570F';
+        } else if( doors[ i ] !== 'car' && choice === 'door-' + i ) {
+          document.getElementById( 'door-' + i ).style.background = '#DA0000';
+        } else {
+          document.getElementById( 'door-' + i ).style.background = '#875E5E';
+        }
+
+      };
+    }
+
+    function clear () {
+      firstChosenDoor = null;
+      secondChosenDoor = null;
+      choice = null;
+      doors = generateDoors();
+      //close all doors
+      for ( i = 0; i < doors.length; i++ ) {
+        document.getElementById( 'door-' + i ).style.background = '#70401C';
+      }
+    }
+
+    document.getElementById( 'stage' ).onclick = function( event ) {
+
+      if( !( /door-[0-9]/g.test( event.toElement.id ) ) ){
+        return false;
+      };
+
+      if( !firstChosenDoor ){
+        firstChosenDoor = event.toElement.id;
+        chooseDoor( firstChosenDoor );
+      } else if ( !secondChosenDoor ){
+        secondChosenDoor = event.toElement.id;
+        chooseDoor( secondChosenDoor );
+      }
+
+      choice = event.toElement.id;
+
+      // if(firstChosenDoor && secondChosenDoor){
+      //   openDoors();
+      // }
+
+    };
+
+    document.getElementById( 'new' ).onclick = function(){
+      clear();
+    };
+
+    document.getElementById( 'open' ).onclick = function(){
+      if ( !choice ){
+        return false;
+      }
+
+      openDoors();
+
+    };
+
+    return{
+      init: function() {
+        doors = generateDoors();
+      }
+    }
+
+  }());
+
+  Monty.init();
+
+}());
