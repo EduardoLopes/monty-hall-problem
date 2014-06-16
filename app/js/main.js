d54e896afc11ba17d5c61e6e5cd670f6ba5f5241
Eduardo Lopes <EduardoLopesDS@gmail.com>
2014-06-16 18:08:21 -0300

    Add a log and fix some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,8 @@
         doors,
         choice,
         canOpen = [],
-        firstOpened;
+        firstOpened,
+        allDoorsOpen = false;
 
     function random( min, max ) {
       return ( min + ( Math.random() * ( max - min ) ) );
@@ -35,34 +36,39 @@
       document.getElementById( id ).classList.remove( class_ );
     };
 
+    function changeText(element, text){
+      document.getElementById(element).innerHTML = '';
+      document.getElementById(element).appendChild( document.createTextNode( text ) );
+    };
+
     function generateDoors () {
       return [ 'car', 'zonk', 'zonk' ].sort(function() {
         return Math.round( random(-1, 2) );
       });
-    }
+    };
 
     function chooseDoor( id ) {
       changeBorder( id, '#00A9C5' );
       if( secondChosenDoor ){
         removeBorder( firstChosenDoor, '#00A9C5' );
       }
-    }
+    };
 
     function openFirstDoor(){
       canOpen = [];
 
       addClass('bar', 'active-bar');
+      for ( i = 0; i < doors.length; i++ ) {
+        if(doors[i] === 'zonk' && firstChosenDoor !== 'door-'+i){
+          canOpen.push(i);
+        }
+      }
 
       setTimeout(function () {
-
-        for ( i = 0; i < doors.length; i++ ) {
-          if(doors[i] === 'zonk' && firstChosenDoor !== 'door-'+i){
-            canOpen.push(i);
-          }
-        }
         firstOpened = 'door-'+ canOpen[ Math.round( random( 0, canOpen.length - 1 ) ) ];
         changeBackground( firstOpened , '#875E5E' );
-
+        addLogMessage('Door number ' + (+firstOpened.match(/[0-9]/g) + 1) + ' have a zonk!');
+        changeText(firstOpened, 'zork');
         removeClass('bar', 'active-bar');
 
       }, 1000);
@@ -74,32 +80,41 @@
 
         if( doors[ i ] === 'car' && choice === 'door-' + i ){
           changeBackground( 'door-' + i , '#08B400' );
+          changeText('door-' + i, 'car');
+          addLogMessage('You win! Door number '+ (i + 1) +' have a car');
         } else if( doors[ i ] === 'car' ) {
           changeBackground( 'door-' + i , '#00570F' );
+          changeText('door-' + i, 'car');
+          addLogMessage('You lose! Door number '+ (i + 1) +' have a car and you choose door number ' + (+choice.match(/[0-9]/g) + 1));
         } else if( doors[ i ] !== 'car' && choice === 'door-' + i ) {
           changeBackground( 'door-' + i , '#DA0000' );
+          changeText('door-' + i, 'zork');
         } else {
           changeBackground( 'door-' + i , '#875E5E' );
+          changeText('door-' + i, 'zork');
         }
 
       };
-    }
+
+      allDoorsOpen = true;
+    };
 
     function clear () {
       firstChosenDoor = null;
       secondChosenDoor = null;
       choice = null;
       firstOpened = null;
+      allDoorsOpen = false;
       doors = generateDoors();
       //close all doors
       for ( i = 0; i < doors.length; i++ ) {
         changeBackground( 'door-' + i , '#70401C' );
         removeBorder( 'door-' + i );
+        changeText('door-' + i, (i + 1));
       }
-    }
+    };
 
     document.getElementById( 'stage' ).onclick = function( event ) {
-      console.log(( /door-[0-9]/g.test( firstOpened ) ));
 
       if( !( /door-[0-9]/g.test( event.toElement.id ) ) ){
         return false;
@@ -108,14 +123,15 @@
       if( !firstChosenDoor ){
         firstChosenDoor = event.toElement.id;
         chooseDoor( firstChosenDoor );
+        addLogMessage('You choose the door number '+ (+firstChosenDoor.match(/[0-9]/g) + 1));
         openFirstDoor();
-      } else if ( !secondChosenDoor && event.toElement.id !== firstOpened && ( /door-[0-9]/g.test( firstOpened ) ) ){
-        console.log(firstOpened);
+        choice = event.toElement.id;
+      } else if ( !secondChosenDoor && event.toElement.id !== firstChosenDoor && event.toElement.id !== firstOpened && ( /door-[0-9]/g.test( firstOpened ) ) && !allDoorsOpen){
         secondChosenDoor = event.toElement.id;
+        addLogMessage('You choose change your door ( '+(+firstChosenDoor.match(/[0-9]/g) + 1)+' ) to the door number '+ (+secondChosenDoor.match(/[0-9]/g) + 1));
         chooseDoor( secondChosenDoor );
+        choice = event.toElement.id;
       }
-
-      choice = event.toElement.id;
 
       // if(firstChosenDoor && secondChosenDoor){
       //   openDoors();
@@ -124,23 +140,41 @@
     };
 
     document.getElementById( 'new' ).onclick = function(){
+      removeLogMessages();
       clear();
     };
 
     document.getElementById( 'open' ).onclick = function(){
-      if ( !choice ){
+      if ( !choice || !firstOpened || allDoorsOpen ){
         return false;
       }
 
+      addLogMessage('Doors are opened');
       openDoors();
+    };
 
+    var logContainer = document.getElementById("log");
+    function addLogMessage(message){
+      var newLi = document.createElement("li"),
+          text = document.createTextNode(message);
+
+      newLi.classList.add('message-log');
+
+      newLi.appendChild(text);
+      logContainer.appendChild(newLi);
+    };
+
+    function removeLogMessages(){
+      while (logContainer.lastChild) {
+        logContainer.removeChild(logContainer.lastChild);
+      }
     };
 
     return{
       init: function() {
         doors = generateDoors();
       }
-    }
+    };
 
   }());
 
